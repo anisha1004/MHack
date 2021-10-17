de9fadc9a24ae2f140115f18127a9c51bd79efeb
anisha1004 <54628217+anisha1004@users.noreply.github.com>
2021-10-17 18:01:27 +0530

    added images

--- frontend/src/Pages/Consult.js
+++ frontend/src/Pages/Consult.js
@@ -1,21 +1,39 @@
 import React from "react";
 import Vector from "../Images/Vector1.svg";
+import Img6 from "../Images/Img6.jpg";
+import Img7 from "../Images/Img7.jpg";
 import "../CSS/Services.css";
 
 function Consut() {
 	return (
 		<div className='service'>
-			<div className='service-head'>
-				Consult the&nbsp;<span style={{ color: "#ec5863" }}> Experts</span>
+			<div className='organise-head-top'>
+				<div>
+					<span>
+						Consult the&nbsp;<span style={{ color: "#ec5863" }}> Experts</span>{" "}
+					</span>
+				</div>
 			</div>
 			<div className='camps'>
 				<div className='camp-item'>
-					<img src={Vector} className='vector' />
-					<span className='camp-text'>Store</span>
+					<img src={Img6} className='vector' />
+					<div className='organise-head'>
+						<span className='camp-text'>VIRTUAL CONSULTATION</span>
+						<span className='organise-content'>
+							Talk to one of our finest experts
+						</span>
+						<span className='organise-content'>virtually.</span>
+					</div>
 				</div>
 				<div className='camp-item'>
-					<img src={Vector} className='vector' />
-					<span className='camp-text'>consult</span>
+					<img src={Img7} className='vector' />
+					<div className='organise-head'>
+						<span className='camp-text'>NEARBY EXPERTS</span>
+						<span className='organise-content'>
+							Look for a professional nearby and
+						</span>
+						<span className='organise-content'>book his appointment.</span>
+					</div>
 				</div>
 			</div>
 		</div>
--- frontend/src/Pages/Services.js
+++ frontend/src/Pages/Services.js
@@ -1,25 +1,47 @@
 import React from "react";
 import Vector from "../Images/Vector1.svg";
+import Img4 from "../Images/Img4.jpg";
+import Img5 from "../Images/Img5.jpg";
 import "../CSS/Services.css";
 
 function Services() {
 	return (
 		<div className='service'>
-			<div className='service-head'>
-				Our&nbsp;<span style={{ color: "#ec5863" }}> Services</span>
+			<div className='organise-head-top'>
+				<div>
+					<span>
+						Our&nbsp;<span style={{ color: "#ec5863" }}> Services</span>{" "}
+					</span>
+				</div>
 			</div>
 			<div className='camps'>
 				<div className='camp-item'>
-					<img src={Vector} className='vector' />
-					<span className='camp-text'>Store</span>
+					<img src={Img5} className='vector' />
+					<div className='organise-head'>
+						<span className='camp-text'>STORE RECORDS</span>
+						<span className='organise-content'>
+							Keep all your records securely
+						</span>
+						<span className='organise-content'>and access them anytime.</span>
+					</div>
+				</div>
+				<div className='camp-item'>
+					<img src={Img4} className='vector' />
+					<div className='organise-head'>
+						<span className='camp-text'>CONSULTANCY</span>
+						<span className='organise-content'>
+							Get the best advice from the
+						</span>
+						<span className='organise-content'>finest doctors.</span>
+					</div>
 				</div>
 				<div className='camp-item'>
 					<img src={Vector} className='vector' />
-					<span className='camp-text'>consult</span>
-				</div>
-				<div className='camp-item'>
-					<img src={Vector} className='vector' />
-					<span className='camp-text'>nearby</span>
+					<div className='organise-head'>
+						<span className='camp-text'>CONRTIBUTE</span>
+						<span className='organise-content'>Give something back to</span>
+						<span className='organise-content'>the society.</span>
+					</div>
 				</div>
 			</div>
 		</div>
